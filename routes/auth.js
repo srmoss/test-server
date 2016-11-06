6a255a1bda6e60b684b8ab8be25bdbc9172341a3
Scott Moss <scott.ryan.moss@gmail.com>
2016-11-05 23:29:58 -0500

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var fork = require('child_process').fork;
+var cron = require('cron');
+var router = express.Router();
+
+/* POST auth. */
+router.post('/', function(req, res, next) {
+  if (req.body.key == process.env.AUTH_KEY)
+  {
+    //Authenticated
+    if (req.body.action == 'Authenticate')
+    {
+      this.childMonitor = fork('../cgm-remote-monitor/server.js', [], {execArgv: []});
+      this.job = cron.job('0 * * * * *', function populate () {
+        var childPopulate = fork('../cgm-remote-monitor/testing/populate_rest.js', [], {execArgv: []});
+      });
+      this.job.start();
+      res.render('success', { message: 'Success' });
+    }
+    else if (req.body.action == 'End')
+    {
+      this.childMonitor.kill();
+      this.job.stop();
+      res.render('success', { message: 'Success' });
+    }
+  }
+  else
+  {
+    res.locals.message = 'Unauthorized'
+    res.locals.error = req.app.get('env') === 'development' ? {"status":"500", "stack":"auth.js"} : {};
+    res.status(500);
+    res.render('error');
+  }
+});
+
+module.exports = router;
